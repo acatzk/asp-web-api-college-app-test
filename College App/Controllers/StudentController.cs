34ba882a635a6fb014c26bbba6785fe2feb5f0cd
acatzk <joshuaimalay@gmail.com>
2024-01-21 11:53:07 +0800

    Create Simple Model and Display Students Data

---
+++ College App/Controllers/StudentController.cs
@@ -0,0 +1,31 @@
+﻿using College_App.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace College_App.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Student> GetStudents()
+        {
+            return new List<Student>(){
+                new Student
+                {
+                    Id = 1,
+                    StudentName = "Student 1",
+                    Email = "student1@gmail.com",
+                    Address = "Address 1 frrom stduent"
+                },
+                new Student
+                {
+                    Id = 2,
+                    StudentName = "Student 2",
+                    Email = "student2@gmail.com",
+                    Address = "Address 2 frrom stduent"
+                }
+            };
+        }
+    }
+}
