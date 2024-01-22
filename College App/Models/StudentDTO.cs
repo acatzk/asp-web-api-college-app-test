10609bd39550e33590665c8becfb2c80c94427ab
acatzk <joshuaimalay@gmail.com>
2024-01-22 23:07:01 +0800

    Model Validations

--- College App/Models/StudentDTO.cs
+++ College App/Models/StudentDTO.cs
@@ -1,10 +1,15 @@
-﻿namespace College_App.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace College_App.Models
 {
     public class StudentDTO
     {
         public int Id { get; set; }
+        [Required]
         public string StudentName { get; set; }
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
         public string Address { get; set; }
     }
 }
