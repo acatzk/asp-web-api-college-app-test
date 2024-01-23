0fad31a0b5587d4254bbf0ce6313bd644f54a9c9
acatzk <joshuaimalay@gmail.com>
2024-01-23 11:22:34 +0800

    Apply built in validation Error in the Field

--- College App/Models/StudentDTO.cs
+++ College App/Models/StudentDTO.cs
@@ -1,15 +1,31 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
 
 namespace College_App.Models
 {
     public class StudentDTO
     {
+        [ValidateNever]
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Student name is required")]
+
+        [StringLength(30)]
         public string StudentName { get; set; }
-        [EmailAddress]
+
+        [EmailAddress(ErrorMessage = "Email must be valid")]
         public string Email { get; set; }
-        [Required]
+
+        [Range(10, 20)]
+        public int Age { get; set;  }
+
+        [Required(ErrorMessage = "Address is required")]
         public string Address { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+
+        [Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; }
+
     }
 }
